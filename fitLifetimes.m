1b4df6f10736517e329535218be0cf23767d9f24
racarrasco <rigocs95@gmail.com>
2020-05-05 16:36:11 -0600

    Handle different fitting rountines

--- fitLifetimes.m
+++ fitLifetimes.m
@@ -1,6 +1,7 @@
 function[fitObject, gof, output] = ...
- fitLifetimes(xin,yin, fitAugerOverlap, type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
-  einf, Nc, Nv, ni,G, phi, f1f2, dopingDensity, defectLevel, defectDensity)
+ fitLifetimes(xin,yin, fitAugerOverlap,fixSRH1, type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
+  einf, Nc, Nv, ni,G, phi, f1f2, dopingDensity, defectLevel, defectDensity,...
+  defectLevel2, defectDensity2)
 
 
 % Calculate all of the lifetime components and add them together the rates
@@ -12,37 +13,156 @@ function[fitObject, gof, output] = ...
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%yin   =  this will be a 1 x n x m matrix, where n is the length of the
-%temperature array in which the measurements were performed, and m is the
-%number of excitations performed. However, in order to perform fitting,the
-%input needs to by and n x m matrix
+%yin   =  the data we will be performing on the fitting
 
 %These will be the fit parameters, and MATLAB arranges the fit parameters
 %alphabetically, not in the order in which they appear.
 
-guess = [dopingDensity, defectLevel, defectDensity];
-if(fitAugerOverlap) 
-    ft = fittype('calculateTotalLifetime(x, a, b, c, d, e, f, g, h, k, l, m, n, o, p, q, r)',...
-    'problem',['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'k'; 'l'; 'm'; 'n']);
-   
-    [fitObject, gof, output] = fit(xin',yin',ft,...
-    'problem',{type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
-    einf, Nc, Nv, ni, G, phi},...
-    'Lower', [0.01 , 0.00001, -1, 0],...
-    'Upper', [0.3 , 100000, 0, inf], ...
-    'Startpoint', guess);  
+
+% There are a lot of forks due to the multiple checkboxes
+% Case 1: fit Bloch overlap, doping, defect1 level and defect1 density,
+% there is no secondary SRH component
+
+% Case 2: nargin == 20, but Bloch checkbox is false
+% fit doping, defect1 level, and defect1 density but do not fit Bloch overlap 
+
+
+
+
+if(nargin == 20) 
+    if(fitAugerOverlap) 
+            % CASE 1
+            % Fit auger overlap, doping, defect level and defect density
+            ft = fittype('calculateTotalLifetime(x, a, b, c, d, e, f, g, h, k, l, m, n, o, p, q, r)',...
+            'problem',['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'k'; 'l'; 'm'; 'n']);
+            guess = [f1f2, dopingDensity, defectLevel, defectDensity]; 
+            
+            [fitObject, gof, output] = fit(xin',yin',ft,...
+            'problem',{type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
+            einf, Nc, Nv, ni, G, phi},...
+            'Lower', [0.01 , 0.00001, -1, 0],...
+            'Upper', [0.3 , 100000, 0, inf], ...
+            'Startpoint', guess);  
+
+     
+    else
+        % CASE 2
+        % Only fit 3 parameters which are doping Density, defect Level, and
+        % defect density, NOT bloch overlap
+        
+        ft = fittype('calculateTotalLifetime(x, a, b, c, d, e, f, g, h, k, l, m, n, o, p, q, r)',...
+        'problem',['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'k'; 'l'; 'm'; 'n'; 'o']);
+        % The guess is doping density, defect Level and defect density
+        guess = [dopingDensity, defectLevel, defectDensity];
+     
+        [fitObject, gof, output] = fit(xin',yin',ft,...
+        'problem',{type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
+        einf, Nc, Nv, ni, G, phi, f1f2},...
+        'Lower', [0.000001, -1, 0],...
+        'Upper', [100000, 0, inf], ...
+        'Startpoint', guess);
+
+    end 
+    
+    
+    
+    
+    
+    
+    
+% We have more input arguments for fitting, which means that we have intentional
+% doping
 else
-    % Only fit 3 parameters which are o, p, and q
-ft = fittype('calculateTotalLifetime(x, a, b, c, d, e, f, g, h, k, l, m, n, o, p, q, r)',...
-    'problem',['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'k'; 'l'; 'm'; 'n'; 'o']);
-
-[fitObject, gof, output] = fit(xin',yin',ft,...
-    'problem',{type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
-    einf, Nc, Nv, ni, G, phi, f1f2},...
-    'Lower', [0.000001, -1, 0],...
-    'Upper', [100000, 0, inf], ...
-    'Startpoint', guess);
-
+    if (fitAugerOverlap)
+        if(fixSRH1) 
+        %FIX SRH1 parameters, fit SRH2 parameters, Bloch overlap parameter,
+        %doping (Fit 4 parameters)
+            ft = fittype('calculateTotalLifetime2(x, a, b, c, d, e, f, g, h, k, l, m, n, o, p, q, r, s, t)',...
+            'problem',['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'k'; 'l'; 'm'; 'n'; 'q'; 'r']);
+        
+        %Fit parameter o(Bloch overlap), p (doping density), s(defectLevel2), t(defectDensity2) 
+             guess = [f1f2, dopingDensity, defectLevel2, defectDensity2];
+        
+            
+            [fitObject, gof, output] = fit(xin',yin',ft,...
+            'problem',{type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
+            einf, Nc, Nv, ni, G, phi,defectLevel, defectDensity},...
+            'Lower', [0.01 , 0.00001, -1, 0],...
+            'Upper', [0.3 , 100000, 0, inf], ...
+            'Startpoint', guess);    
+        else
+        % Fit SRH1, SRH2, Bloch overlap parameter, doping (6 parameters)
+            ft = fittype('calculateTotalLifetime2(x, a, b, c, d, e, f, g, h, k, l, m, n, o, p, q, r, s, t)',...
+            'problem',['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'k'; 'l'; 'm'; 'n']);
+        
+        %Fit parameter o(Bloch overlap) p(doping density) q(defectLevel1) r(defectDensity1)
+        % s(defect level2) t(defect density2)
+            guess = [f1f2, dopingDensity, defectLevel, defectDensity,...
+                defectLevel2, defectDensity2];
+            
+            
+            [fitObject, gof, output] = fit(xin',yin',ft,...
+            'problem',{type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
+            einf, Nc, Nv, ni, G, phi},...
+            'Lower', [0.01 , 0.00001, -1, 0, -1, 0],...
+            'Upper', [0.3 , 100000, 0, inf, 0, inf], ...
+            'Startpoint', guess);  
+            
+        
+        
+        
+        
+            
+        end
+            
+    
+    else 
+        if(fixSRH1)
+             %FIX SRH1 and Bloch overlap. 
+            
+             %FIX SRH1 parameters and Bloch overlap, fit SRH2 parameters,
+        %doping (Fit 3 parameters)
+            ft = fittype('calculateTotalLifetime2(x, a, b, c, d, e, f, g, h, k, l, m, n, o, p, q, r, s, t)',...
+            'problem',['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'k'; 'l'; 'm'; 'n'; 'o'; 'q'; 'r']);
+        
+        %Fit parameter  p (doping density), s(defectLevel2), t(defectDensity2) 
+             guess = [dopingDensity, defectLevel2, defectDensity2];
+        
+             
+            [fitObject, gof, output] = fit(xin',yin',ft,...
+            'problem',{type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
+            einf, Nc, Nv, ni, G, phi,f1f2, defectLevel, defectDensity},...
+            'Lower', [0.00001, -1, 0],...
+            'Upper', [100000, 0, inf], ...
+            'Startpoint', guess);
+            
+        else
+            
+            
+            % Fit SRH1, SRH2, doping, FIX Bloxh overlap parameter (5 parameters)
+            ft = fittype('calculateTotalLifetime2(x, a, b, c, d, e, f, g, h, k, l, m, n, o, p, q, r, s, t)',...
+            'problem',['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'k'; 'l'; 'm'; 'n';'o']);
+        
+        %Fit parameter  p(doping density) q(defectLevel1) r(defectDensity1)
+        % s(defect level2) t(defect density2)
+            guess = [ dopingDensity, defectLevel, defectDensity,...
+                defectLevel2, defectDensity2];
+            
+            
+            [fitObject, gof, output] = fit(xin',yin',ft,...
+            'problem',{type, meStar, mhStar, eg, valenceEdge, conductionEdge,...
+            einf, Nc, Nv, ni, G, phi, f1f2},...
+            'Lower', [ 0.00001, -1, 0, -1, 0],...
+            'Upper', [ 100000, 0, inf, 0, inf], ...
+            'Startpoint', guess);  
+               
+       
+        end
+        
+        
+        
+    end
+        
 end
 
 
@@ -52,16 +172,3 @@ end
 
 
 
-
-
-%{
-fo = fitoptions('Lower', [0.000001, -1, 0],...
-                'Upper', [10000,0, inf],...
-                'StartPoint', guess);
-%}
-    
-%The three parameters will be the doping density, defect level and defect
-%density
-
-% reshape the input to a two-dimensional matrix
-%y = reshape(yin,[size(xin,2), size(yin,3)]);
