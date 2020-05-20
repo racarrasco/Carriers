6aab4904f746223709afa509fa936016c6541d7d
racarrasco <rigocs95@gmail.com>
2020-05-20 14:44:02 -0600

    Added absorption coefficient at band edge

--- extractParameters.m
+++ extractParameters.m
@@ -1,5 +1,15 @@
-function[ mestar, mhstar, eg, nRefractive, kExtinction, photonEnergyum, varshni, einf, f1f2] = extractParameters(bandParams, material)
+function[ mestar, mhstar, eg, nRefractive, kExtinction, photonEnergyum, varshni, einf, f1f2, ag]... 
+    = extractParameters(bandParams, material)
+%Extract the band parameters necessary to calculate the lifetime components
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% Input Parameters %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% bandParams = an empt
+
+
+
+%Point to the file with 
 pathAndFile = "OpticalConstantsDataBase\" + string(material) +".txt";
 
 
@@ -36,14 +46,21 @@ nRefractive = opticalConstantsTable.n;
 
 %row1     =  Band gap in eV (0 K) perhaps
 %row2     =  electron effective mass in units of free electron mass
-%row3     =  gamma1 inverse effective mass parameter 1
-%row4     =  gamma2 inverse effective mass parameter 2
-%row5     =  gamma3 inverse effective mass parameter 3
-%row6     =  Varshni equation parameter1
-%row7     =  Varshni equation parameter2
-%row8     =  high frequency dielectric constant
-%row9     =  Bloch overlap integral (not as a function of temperature)
+%row3     =  gamma1 inverse effective mass parameter 1 Vurgaftman review
+%row4     =  gamma2 inverse effective mass parameter 2 Vurgaftman review
+%row5     =  gamma3 inverse effective mass parameter 3 Vurgaftman review
 
+%row6     =  Varshni equation parameter1 Vurgaftman review
+%row7     =  Varshni equation parameter2 Vurgaftman review
+%row8     =  high frequency dielectric constant Zollner JVSTB 2019
+%row9     =  Bloch overlap integral (not as a function of temperature) a
+
+
+%row10    =  Absorption coefficient of the material at the band gap energy
+%            Comes from S. T. Schaefer, S. Gao, P. T. Webster, R. R.
+%            Kosireddy, and S. R. Johnson, J. Appl. Phys. vol 27 p 165705
+%            (2020). Absorption edge characteristics of GaAs, GaSb, InAs,
+%            InSb
 eg = bandParams(1);
 mestar  = bandParams(2);
 gamm1 = bandParams(3);
@@ -58,6 +75,7 @@ mhstar = 1/(gamm1 - 2*gamm2);
 varshni = @(T) eg - bandParams(6).*T.^2./(T + bandParams(7));
 einf = bandParams(8);
 f1f2 = bandParams(9);
+ag = bandParams(10);
 
 
      
